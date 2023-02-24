69f76e65fbcf057a66cf85811bbe9305cd322517
TawsifKadir <118630521+TawsifKadir@users.noreply.github.com>
2023-02-24 16:13:48 +0600

    Create main.cpp

---
+++ Task2/main.cpp
@@ -0,0 +1,12 @@
+int main() {
+    // Write C++ code here
+    dynArr<double> arr1(5);double x;
+    for(int i=0;i<5;i++){
+        cin >> x;
+        arr1.setValue(i,x);
+    }
+    arr1.printAll();
+    
+
+    return 0;
+}
