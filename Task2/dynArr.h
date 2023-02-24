bcdcdea34d4d593741fb6832e7c950801d490536
TawsifKadir <118630521+TawsifKadir@users.noreply.github.com>
2023-02-24 16:12:57 +0600

    Update dynArr.h

--- Task2/dynArr.h
+++ Task2/dynArr.h
@@ -1,1 +1,16 @@
 
+template<class T>
+class dynArr
+{
+private:
+    T *data;
+    int size;
+public:
+    dynArr();
+    dynArr(int a);
+    ~dynArr();
+    void setValue(int index,T value);
+    T getValue(int index);
+    void printAll();
+    void allocate(int s);
+};
