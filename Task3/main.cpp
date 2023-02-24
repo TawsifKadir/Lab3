727415516736c1c7128d7d0d30baf9186a33315e
TawsifKadir <118630521+TawsifKadir@users.noreply.github.com>
2023-02-24 16:30:39 +0600

    Update main.cpp

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,1 +1,17 @@
-
+int main() {
+    // Write C++ code here
+    int rows,columns;
+    cout << "Enter number of rows:";
+    cin >> rows;
+    cout << "Enter number of columns:";
+    cin >> columns;
+    dynArr<int> arr1(rows,columns);int value;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=columns;j++){
+            cin >> value;
+            arr1.setValue(i,j,value);
+        }
+    }
+    arr1.printAll();
+    return 0;
+}
